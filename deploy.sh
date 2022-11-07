a50efec6777fdad478ad65115a70b16d863fe492
Вахрамеев Владислав <umperators@yandex.ru>
2022-11-07 10:24:48 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ echo > .nojekyll
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -B main
+git checkout -B master
 git add -A
 git commit -m 'deploy'
 
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:rdjake/Neuromorphic-Todo.git master:gh-pages
 
 cd -
