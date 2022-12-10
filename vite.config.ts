e92a68640b7f108d8a74cb30ee5ba8186328d328
Вахрамеев Владислав <umperators@yandex.ru>
2022-12-10 22:15:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   // base: '/Neuromorphic-Todo/',
+   base: '/Neuromorphic-Todo/',
    plugins: [vue()],
    resolve: {
 		alias: {
